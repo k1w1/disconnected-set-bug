3ee81a6a36ea896d1d29e965aa4b8204f236f1dc
k1w1 <chris@aha.io>
2025-02-05 08:55:07 -0800

    Bug report code

--- stencil.config.ts
+++ stencil.config.ts
@@ -4,23 +4,12 @@ export const config: Config = {
   namespace: 'disconnected-set-bug',
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-    },
-    {
       type: 'dist-custom-elements',
       customElementsExportBehavior: 'auto-define-custom-elements',
       externalRuntime: false,
     },
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
-    },
   ],
   testing: {
-    browserHeadless: "shell",
+    browserHeadless: 'shell',
   },
 };
